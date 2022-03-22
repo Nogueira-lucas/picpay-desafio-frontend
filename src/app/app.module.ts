1e4f14b6c8aa35b56a87d42299459c05b04b1907
fernanda-sayuri <fernanda.sayuri.ito@accenture.com>
2022-03-21 23:35:35 -0300

    Criando Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [	
-    AppComponent,
+    AppComponent, HeaderComponent,
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule, 
+    BrowserAnimationsModule
+    //AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
