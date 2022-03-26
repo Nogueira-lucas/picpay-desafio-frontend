62776f2ac20a802d5c4b18fddd81f0c6bacd41ef
guimingardi <guimingardi@gmail.com>
2022-03-26 17:47:52 -0300

    config rotas

--- desafio-front-end/src/App.js
+++ desafio-front-end/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Routes from './routes'
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 
-function App() {
+const customHistory = createBrowserHistory({ basename: '/' })
+customHistory.listen(location => {
+  window.ineum('page', location.pathname)
+})
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router history={customHistory}>
+      <Routes />
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ desafio-front-end/src/routes/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Login from '../components/pages/Login'
+
+const Routes = () => (
+  <Switch>
+	<Route exact path='/' component={Login} />
+  </Switch>
+)
+
+export default Routes
