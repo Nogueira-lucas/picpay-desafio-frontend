fad28520ca531507293ed5700a186bcba17c0ba9
Sidnei <sidnei.teixeira@outlook.com>
2022-03-30 15:45:02 -0300

    chore(payment): adicionado input filter

--- src/app/modules/payments/components/task-list/task-list.component.ts
+++ src/app/modules/payments/components/task-list/task-list.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { Observable, of } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { catchError, map } from "rxjs/operators";
+
 
 import { PaymentsService } from "../../../../services/payments.service";
 import { ErrorDialogComponent } from "../../../shared/components/error-dialog/error-dialog.component";
 import { Task } from "../../models/task";
 import { DeletePaymentComponent } from "../delete-payment/delete-payment.component";
-import { EditPaymentComponent } from '../edit-payment/edit-payment.component';
+import { EditPaymentComponent } from "../edit-payment/edit-payment.component";
 
 @Component({
   selector: "app-task-list",
@@ -17,17 +18,18 @@ import { EditPaymentComponent } from '../edit-payment/edit-payment.component';
 export class TaskListComponent implements OnInit {
   public tasks$: Observable<Task[]>;
 
-  public displayedColumns = ["name", "title", "date", "value", "isPayed","id"];
+  public displayedColumns = ["name", "title", "date", "value", "isPayed", "id"];
 
   constructor(
     private paymentService: PaymentsService,
     public dialog: MatDialog
   ) {
     this.tasks$ = this.paymentService.getTasks().pipe(
+      map((res) => {
+        return res.splice(0, 5);
+      }),
       catchError((err) => {
-        this.onError(
-          'Não foi possível carregar os dados.'
-        );
+        this.onError("Não foi possível carregar os dados.");
         console.error(err);
         return of([]);
       })
@@ -39,16 +41,16 @@ export class TaskListComponent implements OnInit {
       data: errorMsg,
     });
   }
-  
+
   deletePayment(data: Task) {
     this.dialog.open(DeletePaymentComponent, {
-      data 
+      data,
     });
   }
 
   editPayment(data: Task) {
     this.dialog.open(EditPaymentComponent, {
-      data 
+      data,
     });
   }
 
