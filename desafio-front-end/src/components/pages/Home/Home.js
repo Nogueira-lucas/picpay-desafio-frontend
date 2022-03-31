5b091faae7b0459942cd6799325f6c536f4a6a15
guimingardi <guimingardi@gmail.com>
2022-03-31 14:39:09 -0300

    estrutura da home

--- desafio-front-end/src/components/pages/Home/Home.js
+++ desafio-front-end/src/components/pages/Home/Home.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import Navbar from '../../Navbar/Navbar'
+import {
+  Wrapper,
+  ContainerInformations,
+  Header
+} from './Home.style'
+import Title from '../../Title/Title'
+import { TITLE_PAGES } from '../../../config/constants'
+import { Button } from '@mui/material'
+
+const { MY_PAYMENTS } = TITLE_PAGES
 
 const Home = () => {
-  return 'oii'
+  return (
+    <Wrapper>
+      <Navbar />
+      <ContainerInformations>
+        <Header>
+          <Title title={MY_PAYMENTS} />
+          <Button variant='contained' >ADICIONAR PAGAMENTO</Button>
+        </Header>
+      </ContainerInformations>
+    </Wrapper>
+  )
 }
 
 export default Home
---
+++ desafio-front-end/src/components/pages/Home/Home.style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const ContainerInformations = styled.div`
+  width: 100%;
+  margin: 0 auto;
+`
+export const Wrapper = styled.div`
+  flex-direction: column;
+  width: 100%;
+`
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  button {
+    max-width: 230px;
+    margin-top: 24px;
+    height: 36px;
+  }
+`
