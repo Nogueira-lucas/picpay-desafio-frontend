192d4b548aca260d3d5455921ceb1d98f1fe4f14
guimingardi <guimingardi@gmail.com>
2022-04-03 18:42:11 -0300

    add funcionalidade de logout

--- desafio-front-end/src/components/Navbar/Navbar.js
+++ desafio-front-end/src/components/Navbar/Navbar.js
@@ -2,14 +2,22 @@ import React from 'react'
 import {
   Wrapper
 } from './Navbar.style'
+import Button from '@mui/material/Button'
 import { ReactComponent as Logo } from '../../assets/nav-logo.svg'
 import { ReactComponent as Profile } from '../../assets/profile.svg'
 
 const Navbar = () => {
+
+  const logout = () => {
+    localStorage.removeItem('hasPermition')
+    window.location.assign('/')
+  }
+
   return (
     <Wrapper>
       <Logo />
       <Profile />
+      <Button onClick={() => logout()} sx={{color: 'white'}}>Sair</Button>
     </Wrapper>
   )
 }
