b37531c20b36866d9ca9c8023861a95ce3582e71
Carlos Novaes <carlos.novaes@b2wdigital.com>
2022-04-13 10:51:40 -0300

    feat: add login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  submitted = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private userService: UserService) {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
+  login(userData: User) {
+    this.submitted = true;
+
+    if (this.form.invalid) return null;
+
+    const { username, password } = userData;
+
+    this.userService.login(username, password);
+  }
+
+  ngOnInit(): void {
+    console.log(this.userService.user);
+  }
 }
