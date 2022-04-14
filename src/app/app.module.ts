fd2c887a7e72b340474a1dd4a1c396d1751e4a3f
Monique Azevedo <monique.azevedo@b2wdigital.com>
2022-04-14 10:39:42 -0300

    fix(component): ajusta header no storybook e remove comentarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/atoms/button/button.component';
 import { TitleComponent } from './components/atoms/title/title.component';
 import { InputComponent } from './components/atoms/input/input.component';
-// import { LogoComponent } from './components/atoms/logo/logo.component';
-// import { ImageComponent } from './components/atoms/image/image.component';
 import { FormLoginComponent } from './components/molecules/form-login/form-login.component';
 
 import { AppRoutingModule } from './app-routing.module';
--- src/app/components/molecules/header/header.stories.ts
+++ src/app/components/molecules/header/header.stories.ts
@@ -1,10 +1,21 @@
-import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 import { Story, Meta } from '@storybook/angular/types-6-0';
+import { componentWrapperDecorator, moduleMetadata } from '@storybook/angular';
 import { HeaderComponent } from './header.component';
+
+import { CommonModule } from '@angular/common';
+import { LogoComponent } from '../../atoms/logo/logo.component';
+import { ImageComponent } from '../../atoms/image/image.component';
 
 export default {
   title: 'Molecules/HeaderComponent',
   component: HeaderComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [LogoComponent, ImageComponent],
+      imports: [CommonModule],
+    }),
+    componentWrapperDecorator(story => story),
+  ]
 } as Meta;
 
 const Template: Story<HeaderComponent> = (args: HeaderComponent) => ({
