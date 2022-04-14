23f7a64bd7513955d52e204bdc88703cf4d1bf40
Carlos Novaes <carlos.novaes@b2wdigital.com>
2022-04-13 23:38:46 -0300

    feat: implementing initial version of the payment service and creatin its model

---
+++ src/app/models/payment.model.ts
@@ -0,0 +1,10 @@
+export class Payment {
+  id?: number;
+  name?: string;
+  username?: string;
+  value?: number;
+  date?: string;
+  image?: string;
+  isPaid?: boolean;
+  title?: string;
+}
--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Payment } from '../models/payment.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaymentService {
+  payments: Payment[];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  loadPayments() {
+    this.http.get<Payment[]>('http://localhost:3000/tasks').subscribe(payments => {
+      this.payments = payments;
+      console.log(this.payments);
+    });
+  }
 }
