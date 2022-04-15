2f9a0d752e12e3a9a4542317caf0316c524c4bfc
felcg <xfelipecg@gmail.com>
2022-04-15 19:47:44 -0300

    feat(table): Add initial table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,34 @@ import { NgModule } from "@angular/core";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatSortModule } from "@angular/material/sort";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/login-form/login-form.component";
 import { LoginPageComponent } from "./pages/login-page/login-page.component";
+import { TableComponent } from "./components/table/table.component";
+
 @NgModule({
-  declarations: [AppComponent, LoginComponent, LoginPageComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    LoginPageComponent,
+    TableComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
