f834c21cfa98958badadaa6d6c1149961a895813
Monique Azevedo <monique.azevedo@b2wdigital.com>
2022-04-15 14:45:42 -0300

    feat(component): finaliza layout page home

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -8,4 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent {
 
 
+  paymentList = {
+    list: [
+      {
+        id: '1',
+        items: [
+          {item: 'Monique', subItem: '@monique'},
+          {item: 'developr'},
+          {item: '23 Abr 2020', subItem: '16:00 AM'},
+          {item: 'R$ 500'},
+          {item: true}
+        ],
+      },
+      {
+        id: '2',
+        items: [
+          {item: 'Claudia', subItem: '@claudia'},
+          {item: 'QA'},
+          {item: '23 Jun 2020', subItem: '11:00 AM'},
+          {item: 'R$ 100'},
+          {item: true}
+        ],
+      },
+      {
+        id: '3',
+        items: [
+          {item: 'Fulano', subItem: '@Fulano'},
+          {item: 'professor'},
+          {item: '30 Jan 2021', subItem: '09:00 PM'},
+          {item: 'R$ 515'},
+          {item: false}
+        ]
+      }
+    ],
+    tableHead: ['Usuário', 'Título', 'Data', 'Valor', 'Pago']
+  }
 }
