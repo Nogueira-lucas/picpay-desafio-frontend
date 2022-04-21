70ecfad6c8faa1e41c50bb83a1e888723f7f7a4e
João M. Gotardelo <joao.mussi@projuris.com.br>
2022-04-20 23:36:46 -0300

    feat(Home): Layout inicial da página home.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ButtonConfig } from '../_components/button/ButtonConfig';
 
-@Component({ templateUrl: 'home.component.html' })
-export class HomeComponent {
+@Component({
+    templateUrl: 'home.component.html',
+    styleUrls: ['home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+    addButtonConfig: ButtonConfig;
 
     constructor() { }
+
+    ngOnInit() {
+        this.addButtonConfig = {
+            label: "Adicionar Pagamento",
+            primary: true
+        }
+    }
+
+    addTask() {
+        // TODO abertura de modal: adicionar tarefa
+    }
 }
