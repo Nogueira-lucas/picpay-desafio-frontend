490ba7ee76be60900a9032c57fc4cf5d3c5dc569
Thiago <thiago_alcaras@hotmail.com>
2022-04-21 15:16:41 -0300

    Layout tabela

--- src/app/pages/components/main/table-content/table-content.module.ts
+++ src/app/pages/components/main/table-content/table-content.module.ts
@@ -1,14 +1,30 @@
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { TableContentRoutingModule } from './table/table-content.routing';
 import { CardsComponent } from './cards/cards.component';
 import { TableContentComponent } from './table-content.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './table/table.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
     CommonModule,
-    TableContentRoutingModule
+    FormsModule,
+    TableContentRoutingModule,
+    //region Material Modules
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule
+    //endregion Material Modules
   ],
   declarations: [
       TableContentComponent,
--- src/app/pages/components/main/table-content/table/table.component.ts
+++ src/app/pages/components/main/table-content/table/table.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { TasksService } from './../../../../../../core/services/tasks.service';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, AfterViewInit  {
 
-  constructor() { }
+  displayedColumns: string[] = ['usuario', 'titulo', 'data', 'valor', 'pago', 'acoes'];
+
+  resultsLength = 0;
+  isLoadingResults = true;
+  isRateLimitReached = false;
+  data: any[] = [];
+  dataConst: any[] = [];
+  dataSource = new MatTableDataSource<Element>(this.data);
+  searchTerm: string = '';
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  constructor(
+    private _httpClient: HttpClient,
+    private _tasksService: TasksService
+  ) { }
 
   ngOnInit() {
+    this.isLoadingResults = true;
+    this._tasksService.getTasks().subscribe(
+      (data: any[]) => {
+        this.data = data;
+        this.dataConst = data;
+        this.dataSource.data = this.data;
+        this.isLoadingResults = false;
+        this.resultsLength = data.length;
+      }
+    )
   }
 
+  ngAfterViewInit() {
+  }
+
+  searchUser(){
+    this.isLoadingResults = true;
+    this.dataSource.data = this.data.filter(item => item.username.includes(this.searchTerm));
+    this.resultsLength = this.dataSource.data.length;
+    this.isLoadingResults = false;
+  }
 }
